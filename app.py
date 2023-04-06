4ab4eed58ee689342d1227defe1f12350a42a81e
Jefe21054(Ivan) <ivaniglesias500@hotmail.com>
2023-04-06 18:03:33 -0500

    URLs dinámicas con Flask

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def index():
         </h1>
     '''
 
-@app.route('/contacto/')
+@app.route('/contact')
 def contact():
     return '''
         <h1 style="font-size: 64px; color: blue">
@@ -18,10 +18,22 @@ def contact():
         </h1>
     '''
 
-@app.route('/nosotros/')
+@app.route('/about')
 def about():
     return '''
         <h1 style="font-size: 64px; color: blue">
             NOSOTROS
         </h1>
     '''
+
+@app.route('/saludo/<name>/<int:sueldo>')
+@app.route('/saludo/<name>')
+@app.route('/saludo')
+def greetings(name=None, sueldo=None):
+    print(name)
+    if name != None:
+        if sueldo is not None:
+            return f'<h1 style="font-size: 64px; color: blue">Hola {name}, tu sueldo es: {(sueldo)*2}</h1>'
+        return f'<h1 style="font-size: 64px; color: blue">Hola {name}</h1>'
+    else:
+        return f'<h1 style="font-size: 64px; color: blue">Hola Desconocido</h1>'
