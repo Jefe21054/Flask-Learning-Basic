ae32fc5910a57d936961dfbec24d02f00af7745f
Jefe21054(Ivan) <ivaniglesias500@hotmail.com>
2023-04-07 15:12:17 -0500

    Renderizando formularios

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,redirect,url_for,request,\
     render_template,flash,session
+from forms import LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'so-S3cr3t!'
@@ -26,14 +27,15 @@ def about():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
-    if request.method == 'POST':
+    form = LoginForm()
+    if form.validate_on_submit() and request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         if username and password:
             session['username'] = username
             return redirect(url_for('dashboard',name=username))
         flash('Por favor, ingresa correctamente tus datos.','error')
-    return render_template('login.html')
+    return render_template('login.html',form=form)
 
 @app.route('/dashboard/<name>')
 @app.route('/dashboard')
--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import DataRequired,Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Nombre: ',validators=[DataRequired()])
-    password = PasswordField('Clave: ',validators=[DataRequired(),Length(min=4,max=16)])
+    username = StringField('Nombre: ',validators=[DataRequired(message='Ingresa tu nombre')])
+    password = PasswordField('Clave: ',validators=[DataRequired(message='Ingresa tu clave'),Length(min=4,max=16)])
     submit = SubmitField('Enviar')
