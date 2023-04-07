8c65a9e925a79018bcdca3bd6983195d500e1321
Jefe21054(Ivan) <ivaniglesias500@hotmail.com>
2023-04-06 20:58:08 -0500

    Métodos HTTP en nuestras rutas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for
+from flask import Flask,redirect,url_for,request
 
 app = Flask(__name__)
 
@@ -26,17 +26,25 @@ def about():
         </h1>
     '''
 
-@app.route('/login/<name>')
-@app.route('/login')
-def login(name=None):
-    if name != None:
-        return redirect(url_for('dashboard',name=name))
-    else:
-        return f'<h1 style="font-size: 64px; color: blue">Por favor Ingresa tu nombre en la URL...</h1>'
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username and password:
+            return redirect(url_for('dashboard', name=username))
+    return ''' 
+        <form method="POST" style="background-color: #e6e6e6;" >
+        <h3>Ingresa tus datos</h3>
+            <input type="text" name="username" /> <br /> <br />
+            <input type="password" name="password" /> <br /> <br />
+            <button type="submit">Enviar</button>
+        </form>
+    '''
 
 @app.route('/dashboard/<name>')
 @app.route('/dashboard')
 def dashboard(name=None):
     if name is not None:
-        return f'<h1>Bienvenido {name} a tu Dashboard!</h1>'
+        return f'<h1>Bienvenid@ {name} a tu Dashboard!</h1>'
     return redirect(url_for('login'))
